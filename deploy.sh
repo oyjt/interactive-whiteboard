2746f87e1eb2592b597b5d5ded3008582438597c
oyjt <oyjt001@gmail.com>
2023-04-18 14:59:57 +0800

    auto deploy

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,12 @@ set -x  # 这里是为了看错误日志
 pnpm build
 
 # 进入打包后的文件夹
-cd dist
+# cd dist
 
-git init
+# git init
 git add -A
 git commit -m 'auto deploy'
 
 # 将打包后的文件推送到指定分支
 
-git push -f https://github.com/oyjt/interactive-whiteboard.git main:gh-pages
+git push -u origin main
